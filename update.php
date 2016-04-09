0f3e5596c461ba842d8df3e9da9f6f244b142a06
segler-alex <segler_alex@web.de>
2016-04-09 13:19:15 +0200

    really fix homepage links

--- update.php
+++ update.php
@@ -45,8 +45,8 @@ function updateWebpages($db)
             $url = FixUrl($url);
             if ($url !== $row['Homepage']) {
                 echo 'fix homepage ('.$row['StationID'].' - '.$row['Name'].'):'.$row['Homepage'].' -> '.$url."<br/>";
-                // $stmt = $db->prepare('UPDATE Station SET Homepage=:homepage WHERE StationID='.$row['StationID']);
-                // $stmt->execute(['homepage' => $url]);
+                $stmt = $db->prepare('UPDATE Station SET Homepage=:homepage WHERE StationID='.$row['StationID']);
+                $stmt->execute(['homepage' => $url]);
             }
         }
     }
