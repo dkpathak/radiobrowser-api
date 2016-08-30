238acbe58eb3e9f0bf18fc566a465f75cda3ac1b
segler-alex <segler_alex@web.de>
2016-08-31 00:05:54 +0200

    delete sooner

--- update.php
+++ update.php
@@ -59,9 +59,9 @@ function dbCleanup($db){
     // delete clicks older than 30 days
     $db->query('DELETE FROM StationHistory WHERE TIME_TO_SEC(TIMEDIFF(Now(),Creation))>60*60*24*30;');
     // delete stations that never worked after 30 days
-    $db->query('DELETE FROM Station WHERE LastCheckOKTime IS NULL AND LastCheckOK=0 AND TIME_TO_SEC(TIMEDIFF(NOW(),Creation))>60*60*24*30');
+    $db->query('DELETE FROM Station WHERE LastCheckOKTime IS NULL AND LastCheckOK=0 AND TIME_TO_SEC(TIMEDIFF(NOW(),Creation))>60*60*24*5');
     // move stations that have not been working for more than 90 days into deleted list
-    $select_stmt = $db->query('SELECT StationID, Name, Creation, LastCheckOK,LastCheckOKTime FROM Station WHERE LastCheckOKTime IS NOT NULL AND LastCheckOK=0 AND TIME_TO_SEC(TIMEDIFF(NOW(),LastCheckOKTime))>60*60*24*90');
+    $select_stmt = $db->query('SELECT StationID, Name, Creation, LastCheckOK,LastCheckOKTime,datediff(CURDATE(),LastCheckOKTime) AS mydiff FROM Station WHERE LastCheckOKTime IS NOT NULL AND LastCheckOK=0 AND datediff(CURDATE(),LastCheckOKTime)>60 ORDER BY LastCheckOKTime');
     if (!$select_stmt) {
         echo str(mysql_error());
         exit;
